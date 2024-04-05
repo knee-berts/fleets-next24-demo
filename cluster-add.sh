8a391a963876028523922f17956e7bd22ccf9767
Nicholas Eberts <nickeberts@google.com>
2024-04-05 12:36:57 -0400

    bootstrap

--- cluster-add.sh
+++ cluster-add.sh
@@ -4,15 +4,13 @@ set -Eeuo pipefail
 
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
-while getopts p:n:l:c:t:w: flag
+while getopts p:n:l:e: flag
 do
     case "${flag}" in
         p) PROJECT_ID=${OPTARG};;
         n) CLUSTER_NAME=${OPTARG};;
         l) CLUSTER_LOCATION=${OPTARG};;
-        c) CONTROL_PLANE_CIDR=${OPTARG};;
-        t) CLUSTER_TYPE=${OPTARG};;
-        w) APP_DEPLOYMENT_WAVE=${OPTARG};;
+        e) ENV=${OPTARG};;
     esac
 done
 
@@ -20,55 +18,45 @@ echo "::Variable set::"
 echo "PROJECT_ID: ${PROJECT_ID}"
 echo "CLUSTER_NAME: ${CLUSTER_NAME}"
 echo "CLUSTER_LOCATION: ${CLUSTER_LOCATION}"
-echo "CONTROL_PLANE_CIDR:${CONTROL_PLANE_CIDR}"
-echo "CONTROL_TYPE:${CLUSTER_TYPE}"
-echo "APP_DEPLOYMENT_WAVE:${APP_DEPLOYMENT_WAVE}"
 
-REGION=${CLUSTER_LOCATION:0:-2}
 PROJECT_NUMBER=$(gcloud projects describe ${PROJECT_ID} --format="value(projectNumber)")
-echo "REGION:${REGION}"
 echo "PROJECT_NUMBER:${PROJECT_NUMBER}"
 mkdir -p tmp
 
-if [[ ${CLUSTER_TYPE} == "autopilot" ]]; then
-  gcloud beta container --project ${PROJECT_ID} clusters create-auto ${CLUSTER_NAME} \
-    --region ${CLUSTER_LOCATION} \
-    --release-channel "rapid" \
-    --network "gke-poc-toolkit" --subnetwork ${CLUSTER_LOCATION} \
-    --enable-master-authorized-networks \
-    --master-authorized-networks 0.0.0.0/0 \
-    --security-group "gke-security-groups@nickeberts.altostrat.com" 
-  # gcloud container clusters update ${CLUSTER_NAME} --project ${PROJECT_ID} \
-  #   --region ${CLUSTER_LOCATION} \
-  #   --enable-master-global-access 
-  gcloud container clusters update ${CLUSTER_NAME} --project ${PROJECT_ID} \
-    --region ${CLUSTER_LOCATION} \
-    --update-labels mesh_id=proj-${PROJECT_NUMBER}
-else
-  gcloud beta container --project ${PROJECT_ID} clusters create ${CLUSTER_NAME} \
-    --zone ${CLUSTER_LOCATION} \
-    --release-channel "rapid" \
-    --machine-type "e2-medium" \
-    --num-nodes "4" \
-    --gateway-api=standard \
-    --network "gke-poc-toolkit" \
-    --subnetwork ${REGION} \
-    --enable-ip-alias \
-    --enable-autoscaling --min-nodes "3" --max-nodes "15" \
-    --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0 \
-    --labels mesh_id=proj-${PROJECT_NUMBER} \
-    --autoscaling-profile optimize-utilization \
-    --workload-pool "${PROJECT_ID}.svc.id.goog" \
-    --security-group "gke-security-groups@nickeberts.altostrat.com" \
-    --enable-image-streaming --node-locations ${CLUSTER_LOCATION}
-    # --master-ipv4-cidr ${CONTROL_PLANE_CIDR} \
-    # --enable-private-nodes \
-    # --enable-master-authorized-networks \
-    # --master-authorized-networks 0.0.0.0/0 \
-    # --enable-master-global-access \
-fi
+gcloud beta container clusters create ${CLUSTER_NAME} --project ${PROJECT_ID} \
+  --location ${CLUSTER_LOCATION} \
+  --no-enable-basic-auth \
+  --gateway-api=standard \
+  --cluster-version "1.29.2-gke.1521000" \
+  --release-channel "rapid" \
+  --machine-type "e2-standard-4" \
+  --image-type "COS_CONTAINERD" \
+  --disk-type "pd-balanced" --disk-size "100" \
+  --metadata disable-legacy-endpoints=true \
+  --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
+  --num-nodes "3" \
+  --logging=SYSTEM,WORKLOAD,API_SERVER,SCHEDULER,CONTROLLER_MANAGER \
+  --monitoring=SYSTEM,API_SERVER,SCHEDULER,CONTROLLER_MANAGER,STORAGE,POD,DEPLOYMENT,STATEFULSET,DAEMONSET,HPA \
+  --enable-private-nodes --enable-master-global-access --enable-ip-alias \
+  --network "projects/$PROJECT_ID/global/networks/default" \
+  --subnetwork "projects/$PROJECT_ID/regions/$CLUSTER_LOCATION/subnetworks/default" \
+  --enable-intra-node-visibility --cluster-dns=clouddns --cluster-dns-scope=cluster \
+  --default-max-pods-per-node "110" \
+  --security-posture=standard --workload-vulnerability-scanning=enterprise \
+  --enable-dataplane-v2 --enable-master-authorized-networks \
+  --addons HorizontalPodAutoscaling,HttpLoadBalancing,NodeLocalDNS,GcePersistentDiskCsiDriver,BackupRestore,GcpFilestoreCsiDriver,GcsFuseCsiDriver \
+  --enable-autoupgrade --enable-autorepair \
+  --max-surge-upgrade 1 --max-unavailable-upgrade 0 \
+  --labels env=${ENV},region=${CLUSTER_LOCATION} \
+  --binauthz-evaluation-mode=DISABLED \
+  --autoscaling-profile optimize-utilization \
+  --enable-managed-prometheus \
+  --workload-pool "$PROJECT_ID.svc.id.goog" \
+  --enable-shielded-nodes \
+  --security-group "gke-security-groups@gkedemos.joonix.net" \
+  --enable-image-streaming
 
-gcloud projects add-iam-policy-binding ${PROJECT_ID} --role roles/monitoring.viewer --member "serviceAccount:${PROJECT_ID}.svc.id.goog[prod-tools/default]"
+# gcloud projects add-iam-policy-binding ${PROJECT_ID} --role roles/monitoring.viewer --member "serviceAccount:${PROJECT_ID}.svc.id.goog[prod-tools/default]"
 
 gcloud container clusters get-credentials ${CLUSTER_NAME} --zone ${CLUSTER_LOCATION} --project ${PROJECT_ID}
 
@@ -81,38 +69,5 @@ kubectl -n asm-gateways create secret tls edge2mesh-credential \
 --key=tmp/frontend.endpoints.${PROJECT_ID}.cloud.goog.key \
 --cert=tmp/frontend.endpoints.${PROJECT_ID}.cloud.goog.crt --context ${CLUSTER_NAME}
 
-git add . && git commit -m "Added ${CLUSTER_NAME} to the cluster registry folder." && git push origin main
-
 echo "${CLUSTER_NAME} has been deployed and added to the Fleet."
 
-gcloud beta container clusters create ${CLUSTER_NAME} --project ${CLUSTER_PROJECT} \
-  --location ${LOCATION} \
-  --no-enable-basic-auth \
-  --cluster-version "1.29.2-gke.1521000" \
-  --release-channel "rapid" \
-  --machine-type "e2-standard-4" \
-  --image-type "COS_CONTAINERD" \
-  --disk-type "pd-balanced" --disk-size "100" \
-  --metadata disable-legacy-endpoints=true \
-  --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
-  --num-nodes "3" \
-  --logging=SYSTEM,WORKLOAD,API_SERVER,SCHEDULER,CONTROLLER_MANAGER \
-  --monitoring=SYSTEM,API_SERVER,SCHEDULER,CONTROLLER_MANAGER,STORAGE,POD,DEPLOYMENT,STATEFULSET,DAEMONSET,HPA \
-  --enable-private-nodes --enable-master-global-access --enable-ip-alias \
-  --network "projects/$CLUSTER_PROJECT/global/networks/default" \
-  --subnetwork "projects/$CLUSTER_PROJECT/regions/$LOCATION/subnetworks/default" \
-  --enable-intra-node-visibility --cluster-dns=clouddns --cluster-dns-scope=cluster \
-  --default-max-pods-per-node "110" \
-  --security-posture=standard --workload-vulnerability-scanning=enterprise \
-  --enable-dataplane-v2 --enable-master-authorized-networks \
-  --addons HorizontalPodAutoscaling,HttpLoadBalancing,NodeLocalDNS,GcePersistentDiskCsiDriver,BackupRestore,GcpFilestoreCsiDriver,GcsFuseCsiDriver \
-  --enable-autoupgrade --enable-autorepair \
-  --max-surge-upgrade 1 --max-unavailable-upgrade 0 \
-  --labels environment=${ENV},region=${LOCATION} \
-  --binauthz-evaluation-mode=DISABLED \
-  --autoscaling-profile optimize-utilization \
-  --enable-managed-prometheus \
-  --workload-pool "$CLUSTER_PROJECT.svc.id.goog" \
-  --enable-shielded-nodes \
-  --security-group "gke-security-groups@gkedemos.joonix.net" \
-  --enable-image-streaming
---
+++ demo-prep.sh
@@ -0,0 +1,69 @@
+export WORKDIR=`pwd`
+PROJECT_NUMBER=$(gcloud projects describe ${PROJECT_ID} --format="value(projectNumber)")
+
+gcloud services enable --project ${PROJECT_ID} \
+  container.googleapis.com \
+  mesh.googleapis.com \
+  gkehub.googleapis.com \
+  multiclusterservicediscovery.googleapis.com \
+  multiclusteringress.googleapis.com \
+  trafficdirector.googleapis.com \
+  certificatemanager.googleapis.com
+
+gcloud container fleet mesh enable
+
+gcloud container fleet multi-cluster-services enable
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --project ${PROJECT_ID} \
+ --member "serviceAccount:${PROJECT_ID}.svc.id.goog[gke-mcs/gke-mcs-importer]" \
+ --role "roles/compute.networkViewer"
+
+gcloud compute addresses create mcg-ip --global --project ${PROJECT_ID}
+
+export MCG_IP=$(gcloud compute addresses describe mcg-ip --project ${PROJECT_ID} --global --format "value(address)") 
+echo ${MCG_IP}
+
+cat <<EOF > ${WORKDIR}/dns-spec.yaml
+swagger: "2.0"
+info:
+  description: "Cloud Endpoints DNS"
+  title: "Cloud Endpoints DNS"
+  version: "1.0.0"
+paths: {}
+host: "frontend.endpoints.${PROJECT_ID}.cloud.goog"
+x-google-endpoints:
+- name: "frontend.endpoints.${PROJECT_ID}.cloud.goog"
+  target: "${MCG_IP}"
+EOF
+
+gcloud endpoints services deploy ${WORKDIR}/dns-spec.yaml --project ${PROJECT_ID}
+
+gcloud compute security-policies create edge-fw-policy --project ${PROJECT_ID} \
+    --description "Block XSS attacks"
+
+gcloud compute security-policies rules create 1000 --project ${PROJECT_ID} \
+    --security-policy edge-fw-policy \
+    --expression "evaluatePreconfiguredExpr('xss-stable')" \
+    --action "deny-403" \
+    --description "XSS attack filtering"
+
+
+### Need at least one Fleet Membership to assign controller to
+gcloud container fleet ingress enable --project ${PROJECT_ID}
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --project ${PROJECT_ID} \
+    --member "serviceAccount:service-${PROJECT_NUMBER}@gcp-sa-multiclusteringress.iam.gserviceaccount.com" \
+    --role "roles/container.admin"
+
+
+### Do this right before a sync 
+gcloud certificate-manager certificates create mcg-cert --project ${PROJECT_ID} \
+    --domains="frontend.endpoints.${PROJECT_ID}.cloud.goog"
+
+gcloud certificate-manager maps create mcg-cert-map --project ${PROJECT_ID}
+
+gcloud certificate-manager maps entries create mcg-cert-map-entry --project ${PROJECT_ID} \
+    --map="mcg-cert-map" \
+    --certificates="mcg-cert" \
+    --hostname="frontend.endpoints.${PROJECT_ID}.cloud.goog"
+
