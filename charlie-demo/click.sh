dbe5e78d2c02b6357b49e95220d965e8561a3878
Nicholas Eberts <nickeberts@google.com>
2024-04-10 06:31:05 -0700

    bootstrap

--- charlie-demo/click.sh
+++ charlie-demo/click.sh
@@ -1,3 +1,12 @@
+
+# Enable the GKE Enterprise api
+gcloud services disable --project fleet-prod-1 anthos.googleapis.com 
+
+# Create Prod Fleet
+gcloud container fleet create --display-name production --project fleet-prod-1
+gcloud container fleet update --display-name fleet-prod-1 --project fleet-prod-1
+
+# Setup Rollout Sequencing
 gcloud container fleet clusterupgrade update \
     --default-upgrade-soaking=1d \
     --project=fleet-dev-1
